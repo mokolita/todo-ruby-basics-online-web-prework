9f58bcaa3ec191ad6edafb01f81b4f88928fff25
Meg Galvez <meg@oakmaine.com>
2019-02-13 18:21:24 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,31 @@
 def division(num1, num2)
- 
+  num1 = 12
+  num2 = 2
+  num1 / num2
 end
 
-def assign_variable(value)
-  
+def assign_variable(name)
+  name = "Bob"
 end
 
 def argue
   
 end
 
-def greeting
-
+def greeting(greeting, name)
+  name = "Meg"
+  greeting = "Hello"
+  "#{greeting}, #{name}"
 end
 
 def return_a_value
-
+  "Nice"
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
+def pizza_party("cheese")
 
 end
